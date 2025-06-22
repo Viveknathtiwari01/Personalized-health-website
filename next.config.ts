135786af7b46dab6a27917f6464f5a86572ee254
vivek nath tiwari <viveknathtiwari01@gmail.com>
2025-06-22 14:47:52 +0530

    build log fixed

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
